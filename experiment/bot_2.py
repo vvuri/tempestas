3685fefa6b4c67889007bf2c7dbf0cc795dd192d
Yuri Vinogradov <vvuri1978@gmail.com>
2022-11-29 01:14:20 +0300

    Experiment with button

--- experiment/bot_2.py
+++ experiment/bot_2.py
@@ -43,13 +43,31 @@ async def cmd_start(message: types.Message):
 @dp.message_handler(Text(equals="Ok"))
 async def with_puree(message: types.Message):
     await message.reply("Отличный выбор!")
+    await message.answer("Принято", reply_markup=types.ReplyKeyboardRemove())
 
 @dp.message_handler(lambda message: message.text == "Cancel")
 async def without_puree(message: types.Message):
     await message.reply("Так...")
+    await message.reply("Отмена", reply_markup=types.ReplyKeyboardRemove())
 
-# @dp.message_handler(commands=['start', 'weather', 'now'])
-# async def show_weather(message: types.Message):
-#     await message.answer(text="start-weather", reply_markup=inline_keyboard.)
+@dp.message_handler(commands="special")
+async def cmd_special_buttons(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(types.KeyboardButton(text="Запросить геолокацию", request_location=True))
+    keyboard.add(types.KeyboardButton(text="Запросить контакт", request_contact=True))
+    keyboard.add(types.KeyboardButton(text="Создать викторину",
+                                      request_poll=types.KeyboardButtonPollType(type=types.PollType.QUIZ)))
+    await message.answer("Выберите действие:", reply_markup=keyboard)
+
+# inline button with message
+@dp.message_handler(commands="inline")
+async def cmd_inline_url(message: types.Message):
+    buttons = [
+        types.InlineKeyboardButton(text="GitHub", url="https://github.com"),
+        types.InlineKeyboardButton(text="Оф. канал Telegram", url="tg://resolve?domain=telegram")
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    keyboard.add(*buttons)
+    await message.answer("Кнопки-ссылки", reply_markup=keyboard)
 
 executor.start_polling(dp, skip_updates=True)
